1c1275c7eced2fd532ae7b4615e3bb6cb6a00701
Yariv Menachem <yariv220895@gmail.com>
2024-02-04 21:43:14 +0200

    Initial commit

---
+++ src/main/java/com/shiftmanager/ShiftManagerApplication.java
@@ -0,0 +1,13 @@
+package com.shiftmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftmanager/ShiftManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
