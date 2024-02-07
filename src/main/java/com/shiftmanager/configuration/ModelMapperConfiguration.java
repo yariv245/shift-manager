16efaeb4796faa8678666223c64ea98b2a893f47
Yariv Menachem <yariv220895@gmail.com>
2024-02-07 20:54:55 +0200

    added config to model mapper skip null

--- src/main/java/com/shiftmanager/configuration/ModelMapperConfiguration.java
+++ src/main/java/com/shiftmanager/configuration/ModelMapperConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfiguration {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+
+        return modelMapper;
     }
 }
